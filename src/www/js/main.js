1a2edf5b0d33041e4437d2de39a3822cf7d833ec
Krystian Igras <krystian8207@gmail.com>
2020-06-03 16:31:16 +0200

    Trigger position search

--- src/www/js/main.js
+++ src/www/js/main.js
@@ -2,8 +2,7 @@ $( document ).ready(function() {
   $('.position-card').click(function(){
 
     let position = $(this).attr('data-position');
-    $('#players').search('set value', position);
-    $('#players').click();
+    $('#players').search('set value', position).search('search remote', position);
     $('.ui.breadcrumb').find('.section')[0].click();
 
   });
