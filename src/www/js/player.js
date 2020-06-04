0b98b9b29841b45b202111d09409e0bb154d5f19
Marcin Dubel <marcin@appsilon.com>
2020-06-04 11:51:57 +0200

    Set player photo using shiny

--- src/www/js/player.js
+++ src/www/js/player.js
@@ -2,7 +2,6 @@ $( document ).ready(function() {
   $(document).on('click','.main .player-card', function(){
     
     Shiny.setInputValue('main-player', $(this).attr("id"));
-    // click player section
-    $(".levels > div > a")[2].click();
+    $('.ui.breadcrumb').find('.section')[2].click();
   });
 });
