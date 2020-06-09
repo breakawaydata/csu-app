f1b9e66e2d1c3239b19eb2a2d552833226c51b28
Krystian Igras <krystian8207@gmail.com>
2020-06-09 15:52:22 +0200

    Fix typo.

--- src/server.R
+++ src/server.R
@@ -1,6 +1,6 @@
 server <- function(input, output, session) {
 
-  session$senCustomMessage("trackEvent", list(category = "login", action = "state", label = "1"))
+  session$sendCustomMessage("trackEvent", list(category = "login", action = "state", label = "1"))
   menu$init_server("menu", data)
   sidebar$init_server("sidebar")
   main$init_server("main")
