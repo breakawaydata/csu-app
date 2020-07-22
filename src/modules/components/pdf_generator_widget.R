03f78563303acf3aecdc6361c28f9228ce6f9186
pedrocoutinhosilva <pedrocoutinhosilva@gmail.com>
2020-07-22 12:00:29 +0200

    Widget scafolding.

---
+++ src/modules/components/pdf_generator_widget.R
@@ -0,0 +1,47 @@
+import("R6")
+import("utils")
+import("glue")
+import("dplyr")
+import("htmltools")
+import("shiny")
+
+export("pdfDownloader")
+
+ui <- function(id, options) {
+  ns <- NS(id)
+
+  div()
+}
+
+server <- function(input, output, session, state) {
+  ns <- session$ns
+}
+
+pdfDownloader <- R6Class("pdfDownloader",
+  public = list(
+    ui = NULL,
+    server = NULL,
+
+    state = reactiveValues(
+      id = NULL,
+
+      options = list()
+    ),
+
+    initialize = function(id, options = NULL) {
+      isolate({
+        self$state$id <- id
+
+        if(!is.null(options)) self$state$options <- options$style <- modifyList(self$state$options, options)
+      })
+
+      self$ui = function() {
+        ui(id)
+      }
+      self$server = function() {
+        callModule(server, id, self$state)
+      }
+    }
+  )
+)
+pdfDownloader <- pdfDownloader$new
