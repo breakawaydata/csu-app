0b7236c4321b0fc714e6511a7a8165b4b2c5615e
kellisullivan5 <kelli@breakawaydata.com>
2022-03-15 12:46:44 -0700

    Fully upload the first iteration of the stacks foundational site. Take from csu-app that incorporates code from developer regarding the all v team functionality.

--- www/js/main.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-  $('.' + consts.dom.position_card_class).click(function(){
-
-    let position = $(this).attr('data-position');
-    $('#' + consts.search.id).search('set value', position).search('search remote', position);
-    
-    switchToAllView();
-  });
-});
